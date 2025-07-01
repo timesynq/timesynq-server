b4db12b84021cd776d26a49287b582df6ce36246
4acf <acfree332211@gmail.com>
2025-07-01 14:35:47 -0700

    make RedisConnectionProvider lazy initialized and disposable

--- TimesynqServer/Services/Cache/RedisConnectionProvider/RedisConnectionProvider.cs
+++ TimesynqServer/Services/Cache/RedisConnectionProvider/RedisConnectionProvider.cs
@@ -2,20 +2,35 @@
 
 namespace TimesynqServer.Services.Cache.RedisConnectionProvider
 {
-    public class RedisConnectionProvider : IRedisConnectionProvider
+    public class RedisConnectionProvider : IRedisConnectionProvider, IDisposable
     {
 
-        private readonly ConnectionMultiplexer _connection;
+        private readonly Lazy<ConnectionMultiplexer> _connection;
+        private bool _disposed;
 
         public RedisConnectionProvider(IConfiguration configuration)
         {
             string? connectionString = configuration.GetConnectionString("Redis") ?? "";
-            _connection = ConnectionMultiplexer.Connect(connectionString);
+            _connection = new Lazy<ConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(connectionString));
         }
 
         public ConnectionMultiplexer GetConnection()
         {
-            return _connection;
+            return _connection.Value;
+        }
+
+        public void Dispose()
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (_connection.IsValueCreated)
+            {
+                _connection.Value.Dispose();
+                _disposed = true;
+            }
         }
 
     }
